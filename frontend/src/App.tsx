f1098770808e595cbfa38c1b7dd6f700b4ba1c2e
KavyaPandian <kp6986@srmist.edu.in>
2026-07-22 13:06:31 +0530

    button changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -125,7 +125,7 @@ const App: React.FC = () => {
             WebSocket execution enabled
           </span>
 
-          {user && (
+          {user && !currentProblemId && (
             <div style={{ 
               display: 'flex', 
               alignItems: 'center', 
